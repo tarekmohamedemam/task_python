d97c59cc3fecd1d8981ece84fd47bc1f072b60fa
tarekmohamedemam <134011538+tarekmohamedemam@users.noreply.github.com>
2025-07-14 02:39:44 +0300

    Delete email_validation.py

--- email_validation.py
+++
@@ -1,8 +0,0 @@
-def check_email(email):
-    try:
-        if "@" in email and "." in email and email.index("@") < email.rindex("."):
-            return True
-        else:
-            return False
-    except:
-        return False
